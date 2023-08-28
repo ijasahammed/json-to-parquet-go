6943114e8c3f3eac8fd9feb803be63e82983bdcc
Ijas Ahammed <ijasahammed@Ijass-MacBook-Pro.local>
2023-08-28 12:43:20 +0530

    fix list data

--- jsonstruct/jsonstruct.go
+++ jsonstruct/jsonstruct.go
@@ -14,26 +14,32 @@ func BuildJsonStruct(data []map[string]interface{}) map[string]interface{} {
 	output := make(map[string]interface{}, 0)
 	for _, v := range data {
 		for name, val := range v {
-			// field := make(map[string]interface{}, 0)
-			if val == nil {
-				_, output[name] = Read(name, reflect.TypeOf(""), val)
-			} else {
-				_, output[name] = Read(name, reflect.TypeOf(val), val)
+			preValue := make(map[string]interface{}, 0)
+
+			if _, exist := output[name]; exist {
+				keyPre := output[name].(map[string]interface{})
+				if keyPre["type"] == "MAP" {
+					preValue = keyPre["value"].(map[string]interface{})
+				}
+
 			}
-			// field["name"] = name
-			// output[name] = field
+			if val == nil {
+				_, output[name] = Read(name, reflect.TypeOf(""), val, preValue)
+			} else {
+				_, output[name] = Read(name, reflect.TypeOf(val), val, preValue)
+			}
 		}
 	}
 	return output
 }
 
-func Read(name string, t reflect.Type, val any) (string, map[string]interface{}) {
+func Read(name string, t reflect.Type, val any, preValue map[string]interface{}) (string, map[string]interface{}) {
 
 	switch t.Kind() {
 	case reflect.Slice:
 		return "LIST", map[string]interface{}{"type": "LIST", "value": readFromSlice(name, t, val)}
 	case reflect.Map:
-		return "MAP", map[string]interface{}{"type": "MAP", "value": readFromMap(name, t, val)}
+		return "MAP", map[string]interface{}{"type": "MAP", "value": readFromMap(name, t, val, preValue)}
 	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int8:
 		return "INT", map[string]interface{}{"type": "INT", "value": 1}
 	case reflect.Float32, reflect.Float64:
@@ -50,20 +56,34 @@ func Read(name string, t reflect.Type, val any) (string, map[string]interface{})
 func readFromSlice(name string, t reflect.Type, val any) map[string]interface{} {
 
 	resultData := make(map[string]interface{}, 0)
+	preValue := make(map[string]interface{}, 0)
 	new_val := val.([]interface{})
+	var dataType string
 	for _, v := range new_val {
-		_, resultData = Read("element", reflect.TypeOf(v), v)
-		break
+		dataType, resultData = Read("element", reflect.TypeOf(v), v, preValue)
+		if dataType != "MAP" {
+			break
+		} else {
+			preValue = resultData["value"].(map[string]interface{})
+		}
 	}
 	return resultData
 }
 
-func readFromMap(name string, t reflect.Type, val any) map[string]interface{} {
+func readFromMap(name string, t reflect.Type, val any, preValue map[string]interface{}) map[string]interface{} {
 	new_val := val.(map[string]interface{})
-	mapData := make(map[string]interface{}, 0)
+	mapData := preValue
 
 	for key, v := range new_val {
-		_, mapData[key] = Read(key, reflect.TypeOf(v), v)
+		keyPreValue := make(map[string]interface{}, 0)
+		if _, exits := mapData[key]; exits {
+			keyPre := mapData[key].(map[string]interface{})
+			if keyPre["type"] == "MAP" {
+				keyPreValue = keyPre["value"].(map[string]interface{})
+			}
+		}
+		_, mapData[key] = Read(key, reflect.TypeOf(v), v, keyPreValue)
+
 	}
 	return mapData
 
